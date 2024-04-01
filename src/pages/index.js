c328ace9ea0d303f01cde1b7f70d904ec14e90bd
Woody601 <mychalwood@gmail.com>
2024-04-01 15:50:36 -0400

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+import QRScanner from '../components/QRScanner';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <QRScanner />
+    </div>
+  );
+};
+
+export default IndexPage;
