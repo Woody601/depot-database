444e40421855aa378bca3944ecdb91acf8c05690
Woody601 <mychalwood@gmail.com>
2024-04-03 12:55:09 -0400

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Scanner from '../components/Scanner';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>QR and Barcode Scanner</h1>
+      <Scanner />
+    </div>
+  );
+};
+
+export default Home;
